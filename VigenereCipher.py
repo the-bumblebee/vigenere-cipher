9d8b053690929d1ada2c4d7b8b8abe5134c50005
Asif <klmktu.asif@gmail.com>
2019-02-18 23:32:01 +0530

    added CLI functionality

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,3 +1,5 @@
+import argparse
+
 alph = list(map(chr, range(97, 123)))
 
 def encrypt(message, key):
@@ -27,18 +29,40 @@ def decrypt(cipherText, key):
 	return message
 			
 def main():
-	sel = input('Encrypt(1) or decrypt(2):')
-	if sel == '1':
-		message = input('Enter text to be encrypted:')
-		key = input('Enter key:')
-		print('Encrypted message:\n' +  encrypt(message, key))
-	elif sel == '2':
-		message = input('Enter text to be decrypted:')
-		key = input('Enter key:')
-		print('Decrypted message:\n' +  decrypt(message, key))
-		
 	
-main()
+	if args.encrypt_flag == None and args.message == None and args.key == None:
+		sel = input('Encrypt(1) or decrypt(2):')
+		if sel == '1':
+			message = input('Enter text to be encrypted:')
+			key = input('Enter key:')
+			print('Encrypted message:\n' +  encrypt(message, key))
+		elif sel == '2':
+			message = input('Enter text to be decrypted:')
+			key = input('Enter key:')
+			print('Decrypted message:\n' +  decrypt(message, key))
+
+	elif args.encrypt_flag == True and args.message != None and args.key != None:
+		message = args.message
+		key = args.key
+		print('Encrypted message:\n' +  encrypt(message, key))
+
+	elif args.encrypt_flag == False and args.message != None and args.key != None:
+		message = args.message
+		key = args.key
+		print('Decrypted message:\n' +  encrypt(message, key))
+
+	else:
+		parser.print_help()
+	
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-d', action = 'store_false', dest = 'encrypt_flag', default = None, help = 'decrypt message')
+	group.add_argument('-e', action='store_true', dest = 'encrypt_flag', help = 'encrypt message')
+	parser.add_argument('-m', '--message', help = 'message to be encrypted or decrypted')
+	parser.add_argument('-k', '--key', help = 'key used for encryption or decryption')
+	args = parser.parse_args()
+	main()
 			
 			
 			
